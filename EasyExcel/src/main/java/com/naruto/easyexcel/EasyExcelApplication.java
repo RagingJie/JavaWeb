93395dd4b2c273d1b98d278c130fdfb9e830babd
Naruto <2045933252@qq.com>
2024-10-04 23:28:17 +0800

    学习easyExcle

---
+++ EasyExcel/src/main/java/com/naruto/easyexcel/EasyExcelApplication.java
@@ -0,0 +1,14 @@
+package com.naruto.easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyExcelApplication.class, args);
+        System.out.println("学习EasyExcel项目Demo启动！！！");
+    }
+
+}
