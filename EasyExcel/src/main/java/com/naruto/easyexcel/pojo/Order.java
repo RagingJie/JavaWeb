7bde3c6ab4501e90479464f76fd7b6646b5a4910
Naruto <2045933252@qq.com>
2024-10-05 01:20:28 +0800

    订单对象

--- EasyExcel/src/main/java/com/naruto/easyexcel/pojo/Order.java
+++ EasyExcel/src/main/java/com/naruto/easyexcel/pojo/Order.java
@@ -1,10 +1,13 @@
 package com.naruto.easyexcel.pojo;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * @Author
@@ -22,16 +25,22 @@ public class Order implements Serializable {
     // 订单id
     private Integer id;
     // 订单编号
+    @ExcelProperty("订单编号")
     private String orderNumber;
-    // 创建时间
-    private String createTime;
-    // 更新时间
-    private String updateTime;
     // 会员id
+    @ExcelProperty("会员id")
     private String memberId;
     // 订单金额
+    @ExcelProperty("订单金额")
     private BigDecimal price;
     // 子订单id
+    @ExcelProperty("子订单id")
     private Integer kidOrderId;
+    // 创建时间
+    @ExcelProperty("创建时间")
+    private Date createTime;
+    // 更新时间
+    @ExcelProperty("更新时间")
+    private Date updateTime;
 
 }
