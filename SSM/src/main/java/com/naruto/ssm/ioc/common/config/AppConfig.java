e4bbaaa14da8b487fa30f11c039130d554c18401
Naruto <2045933252@qq.com>
2024-10-29 23:59:30 +0800

    配置类

---
+++ SSM/src/main/java/com/naruto/ssm/ioc/common/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.naruto.ssm.ioc.common.config;
+
+import com.naruto.ssm.ioc.dao.Car;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @Author
+ * @Date 2024/10/25 2:12
+ * @Description:
+ */
+//@Import(Car.class)
+@Configuration
+public class AppConfig {
+
+}
