24dd682625b53d106ed15798f7d42045ff71dd91
Naruto <2045933252@qq.com>
2024-11-03 21:25:12 +0800

    AopApplication启动类

---
+++ SSM/src/main/java/com/naruto/ssm/aop/AopApplication.java
@@ -0,0 +1,17 @@
+package com.naruto.ssm.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author
+ * @Date 2024/11/3 3:08
+ * @Description:
+ */
+@SpringBootApplication
+public class AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopApplication.class, args);
+    }
+}
