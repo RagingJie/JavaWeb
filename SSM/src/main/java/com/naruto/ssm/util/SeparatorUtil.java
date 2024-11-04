3b33bd280938b1b89c16b5d2ac16e316eb26357a
Naruto <2045933252@qq.com>
2024-11-04 14:04:38 +0800

    分割线工具类

---
+++ SSM/src/main/java/com/naruto/ssm/util/SeparatorUtil.java
@@ -0,0 +1,19 @@
+package com.naruto.ssm.util;
+
+/**
+ * @Author
+ * @Date 2024/11/3 23:18
+ * @Description:
+ */
+public abstract class SeparatorUtil {
+
+    /**
+     * @return java.lang.String
+     * @author Naruto
+     * @date 2024/11/3 23:20
+     * @description ===== 分隔符
+     */
+    public static void getSeparator() {
+        System.out.println("\n=======================================================================================================================================\n");
+    }
+}
