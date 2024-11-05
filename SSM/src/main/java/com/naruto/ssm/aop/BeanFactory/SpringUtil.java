38ede95a79384f82eecef65b28b19f8a329a102b
Naruto <2045933252@qq.com>
2024-11-05 11:50:27 +0800

    Spring工具类

---
+++ SSM/src/main/java/com/naruto/ssm/aop/BeanFactory/SpringUtil.java
@@ -0,0 +1,30 @@
+package com.naruto.ssm.aop.BeanFactory;
+
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.TypeUtils;
+
+/**
+ * @Author
+ * @Date 2024/11/5 11:11
+ * @Description: Spring中的工具类
+ */
+public class SpringUtil {
+
+    public void util() {
+
+        // 注解工具类
+        // AnnotationUtils
+
+        // 类工具类
+        // ClassUtils
+
+        // 反射工具类
+        // ReflectionUtils
+
+        // 类型工具类
+        // TypeUtils
+    }
+
+}
