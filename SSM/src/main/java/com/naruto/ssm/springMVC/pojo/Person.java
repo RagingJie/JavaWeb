68316e79d9ea02d52f914a8e84bc79f43f1cb58b
Naruto <2045933252@qq.com>
2024-11-06 21:00:01 +0800

    pojo类

---
+++ SSM/src/main/java/com/naruto/ssm/springMVC/pojo/Person.java
@@ -0,0 +1,46 @@
+package com.naruto.ssm.springMVC.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author
+ * @Date 2024/11/6 16:54
+ * @Description:
+*/
+@Data
+public class Person {
+
+    // username=zhangsan&password=mima&cellphone=651511&agreement=on
+
+    // username=234234&password=234234&cellphone=234234
+    // &address.province=山东省&address.city=济南市&address.area=长清区
+    // &sex=男
+    // &hobby=足球&hobby=篮球
+    // &grade=一年级
+
+
+    // 用户名
+    private String username = "wanglaoji";
+    // 密码
+    private String password;
+    // 手机号
+    private String cellphone;
+    // 是否同意
+    private boolean agreement;
+    // 地址
+    private Address address;
+    // 爱好
+    private String[] hobby;
+    // 年级
+    private String grade;
+
+}
+
+@Data
+class Address {
+    private String province;
+    private String city;
+    private String area;
+}
