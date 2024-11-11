50599642329f426dae140f73348135715a1b4195
Naruto <2045933252@qq.com>
2024-11-11 14:24:08 +0800

    自定义校验注解

---
+++ SSM/src/main/java/com/naruto/ssm/springMVC_practise/common/annotation/Gender.java
@@ -0,0 +1,25 @@
+package com.naruto.ssm.springMVC_practise.common.annotation;
+
+import com.naruto.ssm.springMVC_practise.common.validator.GenderValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * @Author
+ * @Date 2024/11/11 13:56
+ * @Description: 自定义校验性别注解
+ */
+@Documented
+@Constraint(validatedBy = {GenderValidator.class})  // 校验器去真正完成校验功能
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Gender {
+
+    String message() default "{javax.validation.constraints.NotEmpty.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
