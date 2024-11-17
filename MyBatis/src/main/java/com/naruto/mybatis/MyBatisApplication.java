e8dfcf49c19ee70d6e974ca0e1d6a751dddd489f
Naruto <2045933252@qq.com>
2024-11-17 17:51:22 +0800

    完善启动信息

--- MyBatis/src/main/java/com/naruto/mybatis/MyBatisApplication.java
+++ MyBatis/src/main/java/com/naruto/mybatis/MyBatisApplication.java
@@ -1,5 +1,6 @@
 package com.naruto.mybatis;
 
+import com.naruto.mybatis.utils.SeparatorUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,10 @@ public class MyBatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyBatisApplication.class, args);
+
+        SeparatorUtil.getSeparator();
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t Mybatis练习项目启动........");
+        SeparatorUtil.getSeparator();
     }
 
 }
