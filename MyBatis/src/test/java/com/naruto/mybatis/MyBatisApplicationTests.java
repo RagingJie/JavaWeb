0ba61d874e01b3a7d1cea0d5ba640e609cfdc6e1
Naruto <2045933252@qq.com>
2024-11-17 17:50:57 +0800

    测试

--- MyBatis/src/test/java/com/naruto/mybatis/MyBatisApplicationTests.java
+++ MyBatis/src/test/java/com/naruto/mybatis/MyBatisApplicationTests.java
@@ -1,13 +1,21 @@
 package com.naruto.mybatis;
 
+import com.naruto.mybatis.mapper.EmpMapper;
+import com.naruto.mybatis.model.pojo.Emp;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyBatisApplicationTests {
 
+    @Autowired
+    private EmpMapper empMapper;
+
     @Test
     void contextLoads() {
+        Emp emp = empMapper.getEmpById(3);
+        System.out.println("员工信息 => " + emp);
     }
 
 }
