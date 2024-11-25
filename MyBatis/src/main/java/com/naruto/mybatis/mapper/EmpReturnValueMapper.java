e7b7cd085ce7b52a2cc92ec878e4cb7533e51125
Naruto <2045933252@qq.com>
2024-11-26 00:33:40 +0800

    返回结果类型

---
+++ MyBatis/src/main/java/com/naruto/mybatis/mapper/EmpReturnValueMapper.java
@@ -0,0 +1,22 @@
+package com.naruto.mybatis.mapper;
+
+import com.naruto.mybatis.model.pojo.Emp;
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface EmpReturnValueMapper {
+
+    Long getCount();
+
+    BigDecimal getSalaryById(Integer id);
+
+    List<Emp> getAll();
+
+    @MapKey("id")  // 实际保存的不是Emp对象，是 HashMap
+    Map<Integer, Emp> getAllMap();
+}
---
+++ MyBatis/src/test/java/com/naruto/mybatis/ReturnValueTest.java
@@ -0,0 +1,46 @@
+package com.naruto.mybatis;
+
+import com.naruto.mybatis.mapper.EmpReturnValueMapper;
+import com.naruto.mybatis.model.pojo.Emp;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+class ReturnValueTest {
+
+    @Autowired
+    EmpReturnValueMapper empReturnValueMapper;
+
+    @Test
+    public void testReturnValue() {
+        Long count = empReturnValueMapper.getCount();
+        System.out.println("员工数量 => " + count);
+
+        BigDecimal salary = empReturnValueMapper.getSalaryById(3);
+        System.out.println("3号员工工资 => " + salary);
+    }
+
+    @Test
+    public void getAll() {
+        List<Emp> all = empReturnValueMapper.getAll();
+        System.out.println(all);
+    }
+
+    @Test
+    public void getAllMap() {
+        Map<Integer, Emp> allMap = empReturnValueMapper.getAllMap();
+        System.out.println("allMap => " + allMap);
+
+        System.out.println(allMap.get(2));
+
+        Emp emp = allMap.get(2);
+        System.out.println(allMap.get(2).getClass());
+//        System.out.println(emp.getClass());
+//        System.out.println(emp.getAge());
+    }
+}
