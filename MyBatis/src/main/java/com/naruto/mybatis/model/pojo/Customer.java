ac8f6c2f8a2b79df69803d2c10899ca9899b38d4
Naruto <2045933252@qq.com>
2024-11-26 23:27:22 +0800

    学习mybatis--顾客类

---
+++ MyBatis/src/main/java/com/naruto/mybatis/model/pojo/Customer.java
@@ -0,0 +1,10 @@
+package com.naruto.mybatis.model.pojo;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private Integer id;
+    private String customerName;
+    private String phone;
+}
