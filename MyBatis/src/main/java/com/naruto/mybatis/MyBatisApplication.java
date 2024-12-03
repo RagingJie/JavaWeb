2b050534b0e0d9c1232ba50486505f92fa1efacf
Naruto <2045933252@qq.com>
2024-12-03 09:49:47 +0800

    开启事务

--- MyBatis/src/main/java/com/naruto/mybatis/MyBatisApplication.java
+++ MyBatis/src/main/java/com/naruto/mybatis/MyBatisApplication.java
@@ -3,7 +3,11 @@ package com.naruto.mybatis;
 import com.naruto.mybatis.utils.SeparatorUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
+
+@EnableTransactionManagement
 @SpringBootApplication
 public class MyBatisApplication {
 
