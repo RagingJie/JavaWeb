7fabf150a6f8ad99ed7d1d47649d9ba0c47a8033
Naruto <2045933252@qq.com>
2024-12-04 00:47:37 +0800

    设置分页合理化参数

--- MyBatis/src/main/java/com/naruto/mybatis/config/MyBatisConfig.java
+++ MyBatis/src/main/java/com/naruto/mybatis/config/MyBatisConfig.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Properties;
+
 /**
  * @Auther: Naruto
  * @Date: 2022/10/14 09:06
@@ -18,6 +20,10 @@ public class MyBatisConfig {
         PageInterceptor pageInterceptor = new PageInterceptor();
         // 2、根据需求设置相应的参数
         // ..........
+        // 设置分页合理化参数，默认值为false。当该参数设置为 true 时，pageNum<=0 时会查询第一页， pageNum>pages（超过总数时），会查询最后一页。默认false 时，直接根据参数进行查询
+        Properties properties = new Properties();
+        properties.setProperty("reasonable", "true");
+        pageInterceptor.setProperties(properties);
         return pageInterceptor;
     }
 }
