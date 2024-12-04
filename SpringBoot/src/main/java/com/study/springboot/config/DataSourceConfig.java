2145fa9539f9086f540238c109830b76c97e9356
Naruto <2045933252@qq.com>
2024-12-04 23:51:09 +0800

    学习SpringBoot--通过配置类的方式创建数据源

---
+++ SpringBoot/src/main/java/com/study/springboot/config/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package com.study.springboot.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * 通过配置类的方式创建数据源
+ */
+//@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        return dataSource;
+    }
+}
