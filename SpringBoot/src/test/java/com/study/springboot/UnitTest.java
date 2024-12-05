1774a0328aef420a804c8ac8d98783efd305417c
Naruto <2045933252@qq.com>
2024-12-05 23:00:04 +0800

    学习SpringBoot--单元测试扩展常用注解

--- SpringBoot/src/test/java/com/study/springboot/UnitTest.java
+++ SpringBoot/src/test/java/com/study/springboot/UnitTest.java
@@ -1,15 +1,38 @@
 package com.study.springboot;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Slf4j
 @SpringBootTest
 public class UnitTest {
 
+    @DisplayName("第一个单元测试")
     @Test
     public void contextLoads() {
         log.info("单元测试");
+    }
+
+    @BeforeEach
+    public void before(){
+    	log.info("在每个单元测试之前执行");
+    }
+
+    @AfterEach
+    public void after(){
+        log.info("在每个单元测试之后执行");
+    }
+
+    @BeforeAll
+    public static void beforeAll(){
+        log.info("在所有单元测试之前执行");
+    }
+
+    @Timeout(2L)
+    @Test
+    public void timeout() throws InterruptedException {
+        log.info("超时测试");
+        Thread.sleep(4000);
     }
 }
